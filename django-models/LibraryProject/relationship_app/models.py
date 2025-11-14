bce03c1b6d3c251e561b7f0b89b49cb74840d3b5
Mariam <mariam3maged@gmail.com>
2025-11-14 14:11:55 +0200

    DjangoModels2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField()
+    
+    def __str__(self):
+        return self.name  # Shows author name in admin and shell
 
 class Book(models.Model):
     title = models.CharField(),
     author = models.ForeignKey(Author,on_delete=models.CASCADE,related_name="Books")
+
 
 class Library(models.Model):
     name = models.CharField(),
