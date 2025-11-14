dfab3fdc99cc6959a4bef7ef725f1f32469ec176
Mariam <mariam3maged@gmail.com>
2025-11-14 14:40:06 +0200

    DjangoModels5

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Book,Librarian,Library,Author
 ListOfBooks_specificAuthor = Book.objects.filter(author__name='dd')
 
 
-libraryName = Library.objects.get(name='LibraryXX')
+libraryName = Library.objects.get(name='library_name')
 ListOfBooks = libraryName.books.all()   # books is related_name in class library , that has library and book mapping
 
 librarianName = Librarian.objects.get(library__name='hh')
