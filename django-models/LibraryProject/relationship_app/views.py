869ae603191906d32c2c410caea0875552f296f3
Mariam <mariam3maged@gmail.com>
2025-11-14 16:24:35 +0200

    task 2_2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,15 +6,16 @@ from django.views.generic import DetailView
 
 # Create your views here.
 
+#This view should render a simple text list of book titles and their authors.
 def book_list(request):
-    books = Books.objects.all()
+    books = Book.objects.all()
     book_list = {'books' : books}
 
-    return render(request, 'relationship_app/templates/relationship_app/list_books.html',book_list )
+    return render(request, 'relationship_app/list_books.html',book_list )
 
 class LibraryDetails(DetailView):
     model = Library
-    template_name = 'relationship_app/templates/relationship_app/list_books.html'
+    template_name = 'relationship_app/list_books.html'
     
     def get_context_data(self, **kwargs):
         contextDictionary = super().get_context_data(**kwargs) # default dict value
